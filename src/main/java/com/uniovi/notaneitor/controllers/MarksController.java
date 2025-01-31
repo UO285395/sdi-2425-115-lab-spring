0e0b97eeed985f4a7af8ecd5fb251c926bc0a485
uo285395 <uo285395@uniovi.es>
2025-01-31 11:51:53 +0100

    SDI-2425-115-2.3-Beans, servicios e inyeccion de dependencias

--- src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
+++ src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
@@ -1,5 +1,7 @@
 package com.uniovi.notaneitor.controllers;
 
+import com.uniovi.notaneitor.services.MarksService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.uniovi.notaneitor.entities.*;
 
@@ -7,16 +9,26 @@ import com.uniovi.notaneitor.entities.*;
 @RestController
 public class MarksController {
 
-    @RequestMapping
+    @Autowired
+    private MarksService marksService;
+
+    @RequestMapping("/mark/list")
     public String getList(){
-        return "Getting List";
+        return marksService.getMarks().toString();
     }
+
     @RequestMapping( value = "/mark/add", method = RequestMethod.POST)
     public String setMark(@ModelAttribute Mark mark) {
-        return "Added " + mark.getDescription() +
-                " with score " + mark.getScore()+
-                " id: " + mark.getId();
+        marksService.addMark(mark);
+        return "OK";
     }
+
+//    @RequestMapping( value = "/mark/add", method = RequestMethod.POST)
+//    public String setMark(@ModelAttribute Mark mark) {
+//        return "Added " + mark.getDescription() +
+//                " with score " + mark.getScore()+
+//                " id: " + mark.getId();
+//    }
 
 
     //Busca un parametro id en la URL
@@ -29,8 +41,15 @@ public class MarksController {
     //Se le pasa directamente
     @RequestMapping("/mark/details/{id}")
     public String getDetail(@PathVariable Long id) {
-        return "Getting Details =>"+ id;
+       return marksService.getMark(id).toString();
     }
+
+    @RequestMapping("/mark/delete/{id}")
+    public String deleteMark(@PathVariable Long id) {
+        marksService.deleteMark(id);
+        return "OK";
+    }
+
 
 }
 
