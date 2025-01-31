0efb0aa8ce9190ab9f8093d0659a665c4d507d02
uo285395 <uo285395@uniovi.es>
2025-01-31 08:35:51 +0100

    SDI-2425-115-2.1-Create Spring project

---
+++ src/test/java/com/uniovi/sdi/sdi2425115spring/Sdi2425115SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.sdi2425115spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2425115SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
